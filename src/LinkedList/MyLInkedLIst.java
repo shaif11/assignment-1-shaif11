820f40b61c309749e996a46dc3cedced05a827b7
shaif11 <shaifsiddiqui_cs18@gla.ac.in>
2020-02-24 21:55:07 +0530

    create class main

---
+++ src/LinkedList/MyLInkedLIst.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+import adt.ContactADT;
+
+class MyLinkedLIst<E> implements ContactADT<E> {
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public void search(E item) {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
